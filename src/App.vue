ec730b7c7d04cb33ec2662244b25de34204650bf
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-01-24 18:52:44 +0100

    m

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <v-tab>Starters</v-tab>
         <v-tab>Mains</v-tab>
         <v-tab>Soups</v-tab>
-        <v-tab>Deserts</v-tab>
+        <v-tab>Desertssss</v-tab>
         <v-spacer></v-spacer>
       </v-tabs>
 
