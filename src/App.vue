45c7fbee63a4b3d198c48db187500533ca5c760c
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-01-24 19:01:13 +0100

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app>
-      <v-tabs align-with-title>
-        <v-tab>Starters</v-tab>
-        <v-tab>Mains</v-tab>
-        <v-tab>Soups</v-tab>
-        <v-tab>Deserts</v-tab>
-        <v-spacer></v-spacer>
-      </v-tabs>
-
-      <v-btn depressed> Log in </v-btn>
-    </v-app-bar>
-
+    <Header />
     <v-main>
       <!--  -->
     </v-main>
   </v-app>
 </template>
 <script>
+import header from "./components/header.vue";
 export default {
+  components: {
+    Header: header,
+  },
   data: () => ({}),
 };
 </script>
