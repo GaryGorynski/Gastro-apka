865904aee1d74159dc844ae65e27daa388880c04
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-01-25 11:03:31 +0100

    m (#5)

---
+++ src/components/RecipePopup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Starters.vue
+++ src/views/Starters.vue
@@ -1,11 +1,50 @@
 <template>
   <v-app id="inspire">
     <Header />
-    <v-main>
+    <v-main
+      ><div class="text-center">
+        <v-dialog v-model="dialog" width="550">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              @click="togglePopup"
+              fixed
+              right
+              bottom
+              class="mx-2"
+              fab
+              dark
+              color="green"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon dark> mdi-plus </v-icon>
+            </v-btn>
+          </template>
+
+          <v-card max-width="600">
+            <v-col class="ml-10" cols="12" sm="6">
+              <vcard-title class="text-h5">Add new recipe </vcard-title>
+              <v-text-field label="Title"></v-text-field>
+            </v-col>
+            <v-col class="ml-10" cols="12" md="6">
+              <v-textarea label="Description" value=""></v-textarea>
+            </v-col>
+            <v-col class="d-flex ml-10" cols="12" sm="6">
+              <v-select :items="items" label="Ingredients" dense></v-select>
+              <v-select :items="items" label="Quantity" dense></v-select>
+            </v-col>
+            <v-col class="ml-10" cols="12" md="6">
+              <v-textarea name="input-7-1" label="Method" value=""></v-textarea>
+            </v-col>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="success"> Add recipe </v-btn>
+              <v-btn color="error"> Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
       <RecipeCard />
-      <v-btn fixed right bottom class="mx-2" fab dark color="green">
-        <v-icon dark> mdi-plus </v-icon>
-      </v-btn>
     </v-main>
   </v-app>
 </template>
@@ -13,12 +52,20 @@
 <script>
 import header from "../components/header.vue";
 import recipeCard from "../components/recipeCard.vue";
+
 export default {
   components: {
     Header: header,
     RecipeCard: recipeCard,
   },
-  data: () => ({}),
+  methods: {
+    togglePopup: function () {
+      this.popupOpen = !this.popupOpen;
+    },
+  },
+  data: () => ({
+    popupOpen: false,
+  }),
 };
 </script>
 
