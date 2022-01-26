66fdb3255a0bbc5932b14b0f50e632745c699148
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-01-26 11:53:10 +0100

    segregacja komponentow

---
+++ src/views/tabs/Soups.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="inspire">
+    <Header />
+    <v-main>
+      <RecipeCard />
+      <AddRecipe />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import header from "../../components/header.vue";
+import recipeCard from "../../components/recipe/recipeCard.vue";
+import AddRecipe from "../../components/recipe/AddRecipe.vue";
+export default {
+  components: {
+    Header: header,
+    RecipeCard: recipeCard,
+    AddRecipe: AddRecipe,
+  },
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss"></style>
