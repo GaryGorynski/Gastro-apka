d1cca16c61a304f9f246b87474ab7463e4eb7aaf
GaryGorynski <79517096+GaryGorynski@users.noreply.github.com>
2022-02-04 19:38:46 +0100

    dotenv added

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import vuelidate from "./plugins/vuelidate";
-
+import axios from "axios";
+axios.interceptors.request.use((config) => {
+  const token = process.env.VUE_APP_API_KEY;
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
 Vue.config.productionTip = false;
 
 new Vue({
